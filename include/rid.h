2633189c2c4b358813c8ea2f664e11ff26551091
Mika Tuupola <tuupola@appelsiini.net>
2025-12-14 07:58:41 +0200

    Add convenience umbrella header

---
+++ include/rid.h
@@ -0,0 +1,19 @@
+#ifndef _RID_H
+#define _RID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "rid/header.h"
+#include "rid/basic_id.h"
+#include "rid/location.h"
+#include "rid/operator_id.h"
+#include "rid/self_id.h"
+#include "rid/system.h"
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _RID_H */
