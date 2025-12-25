614db114bf2a7290f1887ab4a7466e1751507edc
Mika Tuupola <tuupola@appelsiini.net>
2025-12-25 16:12:17 +0200

    Add example for message headers

---
+++ examples/message/message.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "rid/message.h"
+
+int
+main(void)
+{
+    /* [full_example] */
+    uint8_t buffer[] = {
+        0xe3, 0xd2, 0x27, 0xcf, 0xfd, 0x7f, 0x00, 0x00,
+        0x42, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00
+    };
+
+    const rid_message_t *message = (const rid_message_t *)buffer;
+
+    rid_protocol_version_t version = rid_get_protocol_version(message);
+    rid_message_type_t type = rid_get_message_type(message);
+
+    printf("Protocol version: %u\n", version);
+    printf("Message type:     %u\n", type);
+    /* [full_example] */
+
+    return 0;
+}
